ee3a4effefaf38afcc30c481a56b804e8cffa52e
Naveen Kumar <naveenkumar.career3@gmail.com>
2026-01-10 20:41:57 +0530

    Dashboard Creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/register";
+import DashBoard from "./pages/DashBoard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 
@@ -10,6 +12,15 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        <Route 
+         path="/dashboard"
+         element ={
+          <ProtectedRoute>
+            <DashBoard />
+          </ProtectedRoute>
+         }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/DashBoard.jsx
@@ -0,0 +1,10 @@
+function DashBoard() {
+    return(
+        <div className='p-6'>
+            <h1 className="text-xl font-semibold">Dashboard</h1>
+            <p>Only logged-in users can see this page</p>
+        </div>
+    );
+}
+
+export default DashBoard;
